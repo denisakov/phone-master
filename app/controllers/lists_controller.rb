9fae704651dfc0d5fc254cb7ab61020d267014af
denisakov <denis.isakov@tut.by>
2017-02-18 14:11:03 +0000

    list view worked

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  # before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   def index
     @lists = List.all
@@ -70,12 +70,12 @@ class ListsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_list
-    #   @list = List.find(params[:id])
-    # end
+    def set_list
+      @list = List.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    # def list_params
-    #   params.require(:list).permit(:title)
-    # end
+    def list_params
+      params.require(:list).permit(:title)
+    end
 end
