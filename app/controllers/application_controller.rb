83c5a5b1de3da1f7bb35fefb2ae17e8f0c0523e5
denisakov <denis.isakov@tut.by>
2017-02-23 14:59:42 +0000

    precompiled assests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   add_flash_types :notice, :error
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   # ApplicationNotAuthenticated = Class.new(Exception)
 
   def handleLogin()
