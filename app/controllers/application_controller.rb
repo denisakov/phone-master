54ae9d57b98b47d2efe6b95944a599e32ee4fe81
denisakov <denis.isakov@tut.by>
2017-02-23 14:21:39 +0000

    login testing 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,27 @@
 class ApplicationController < ActionController::Base
   add_flash_types :notice, :error
   protect_from_forgery with: :exception
-  ApplicationNotAuthenticated = Class.new(Exception)
+  # ApplicationNotAuthenticated = Class.new(Exception)
 
-  rescue_from ApplicationNotAuthenticated do
-    respond_to do |format|
-      # format.json { render json: { errors: [message: "401 Not Authorized"] }, status: 401 }
-      format.html do
-        flash[:error] = "You are not authorized to access this page, please log in"
-        redirect_to '/login'
-      end
-      # format.any { head 401 }
-    end
+  def handleLogin()
+    flash[:error] = "You are not authorized to access this page, please log in"
+    redirect_to '/login'
   end
 
+  # rescue_from ApplicationNotAuthenticated do
+  #   respond_to do |format|
+  #     # format.json { render json: { errors: [message: "401 Not Authorized"] }, status: 401 }
+  #     format.html do
+  #       flash[:error] = "You are not authorized to access this page, please log in"
+  #       redirect_to '/login'
+  #     end
+  #     # format.any { head 401 }
+  #   end
+  # end
+
   def authentication_required!
-    session[:current_user] || raise(ApplicationNotAuthenticated)
+    session[:current_user] || handleLogin()
+    # raise(ApplicationNotAuthenticated)
   end
   
   # rescue_from ActionController::RoutingError do |exception|
