45033821c3fa79a2795ba7c3f4b65973ab26cd3b
denisakov <denis.isakov@tut.by>
2017-02-25 19:24:23 +0000

    routing issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   add_flash_types :notice, :error
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session # :exception
+  
   ApplicationNotAuthenticated = Class.new(Exception)
 
   # def handleLogin()
